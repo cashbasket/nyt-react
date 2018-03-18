21ab36c49bc761082c17e485909fd4881ffb423b
Ryan Machan <rjm21@case.edu>
2018-03-18 00:55:06 -0400

    remove nav component

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import Search from './pages/Search';
 import Saved from './pages/Saved';
 import NoMatch from './pages/NoMatch';
 import Nav from './components/Nav';
+import { FormBtn } from './components/Form';
 
 const App = () => (
   <Router>
     <div>
-      <Nav />
       <Switch>
         <Route exact path="/" component={Search} />
         <Route exact path="/search" component={Search} />
