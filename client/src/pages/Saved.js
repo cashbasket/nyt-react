0d1cc46ecb8aa5a83672496411c1516a3b599734
Ryan Machan <rjm21@case.edu>
2018-03-18 16:41:30 -0400

    updates

--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -9,13 +9,14 @@ import moment from 'moment';
 
 class Saved extends Component {
   state = {
-    articles: []
+    articles: [],
+    loaded: false
   };
 
   loadArticles = () => {
     API.getSavedArticles()
       .then(savedArticles => {
-        this.setState({articles: savedArticles.data});
+        this.setState({articles: savedArticles.data, loaded: true});
       }
       )
       .catch(err => 
@@ -42,34 +43,48 @@ class Saved extends Component {
       <Container>
         <Row>
           <Col className="col-md-12">
-            <Jumbotron>
-              <h1><em>New York Times</em> Article Scrubber<br /><small>&mdash; Saved Articles &mdash;</small></h1>
-              <p className="lead text-center">These are all the articles that have been saved. Feel free to delete whatever you want if you're done reading or if you just can't stand the article anymore; it won't offend anyone.</p>
-              <p className="text-center">(Pro-tip: to get back to the search page, click the button in the lower-right corner!)</p>
-            </Jumbotron>
+            <div class="card article-card bg-secondary text-white">
+              <div class="card-body">
+                <h2 class="text-center"><strong>SAVED ARTICLES</strong></h2>
+              </div>
+            </div>
             {this.state.articles.length ? (
               <ArticleList>
                 {this.state.articles.map((article, index) => (
                   <ArticleItem key={`article-${article._id}`}>
-                    <h3><a href={article.web_url} target="_blank">{article.headline}</a></h3>
-                    <p>{article.byline} <em>(published {moment(article.datePublished).format('M/D/YYYY @ h:mmA')})</em></p>
+                    <h3><a href={article.url} target="_blank">{article.headline}</a></h3>
+                    <p>{article.byline}<br />
+                      <small>Published {moment(article.datePublished).format('M/D/YYYY @ h:mmA')} <em>(and saved {moment(article.dateAdded).format('M/D/YYYY @ h:mmA')})</em></small></p>
                     <p>{article.snippet}</p>
-                    <div className="d-flex w-100 justify-content-between">
-                      <a href={article.url} className="full-article-link" target="_blank"><i className="fas fa-external-link-alt"></i> Read Full Article</a>
-                      <a key={`delete-${index + 1}`} className="delete-article-link" onClick={() => this.deleteArticle(article._id)}><i className="fas fa-trash"></i> Delete This</a>
-                    </div>
+                    <Row>
+                      <Col className="col-md-6">
+                        <a href={article.url} className="full-article-link" target="_blank"><i className="fas fa-external-link-alt"></i> Read Full Article</a>
+                      </Col>
+                      <Col className="col-md-6 col-option">
+                        <a key={`delete-${index + 1}`} className="delete-article-link" onClick={() => this.deleteArticle(article._id)}><i className="fas fa-trash"></i> Delete This</a>
+                      </Col>
+                    </Row>
                   </ArticleItem>
                 ))}
               </ArticleList>
             ) : (
               <Row>
                 <Col className="col-md-12">
-                  <div className="card text-white bg-danger mb-3">
-                    <div className="card-body text-center">
-                      <i className="far fa-frown fa-4x no-results-icon"></i>
-                      <h5 className="card-text">There are currently no saved articles.</h5>
+                  {this.state.loaded ? (
+                    <div className="card text-white bg-danger mb-3">
+                      <div className="card-body text-center">
+                        <i className="far fa-frown fa-4x no-results-icon"></i>
+                        <h5 className="card-text">There are no saved articles.</h5>
+                      </div>
                     </div>
-                  </div>
+                  ) : (
+                    <div className="card bg-light mb-3">
+                      <div className="card-body text-center">
+                        <i className="fas fa-spinner fa-spin fa-4x no-results-icon"></i>
+                        <h5 className="card-text">Loading articles...</h5>
+                      </div>
+                    </div>
+                  )}
                 </Col>
               </Row>
             )}
@@ -77,7 +92,7 @@ class Saved extends Component {
         </Row>
         <div className="action-div">
           <Link to="/">
-            <FormBtn className="btn btn-action btn-view-search btn-lg animated infinite pulse"><i className="fas fa-search fa-2x"></i></FormBtn>
+            <FormBtn className="btn btn-action btn-view-search btn-lg animated infinite pulse"><i className="fas fa-search fa-3x"></i></FormBtn>
           </Link>
         </div>
       </Container>
