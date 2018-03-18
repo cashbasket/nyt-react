6666e08bd76fc2cd8ac8d3245d8a207597f32d8c
Ryan Machan <rjm21@case.edu>
2018-03-18 00:53:56 -0400

    format/style article item component

--- client/src/components/Article/ArticleItem.js
+++ client/src/components/Article/ArticleItem.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import './ArticleItem.css';
 
-export const ArticleItem = props => <li className="article-item">{props.children}</li>;
-
+export const ArticleItem = props => <li className="article-item">
+  <div className="card article-card">
+    <div className="card-body">
+      {props.children}
+    </div>
+  </div>
+</li>;
