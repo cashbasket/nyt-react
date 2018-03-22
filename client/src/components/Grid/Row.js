7778bb51509070283a937d9a0829bc362295415e
Ryan Machan <rjm21@case.edu>
2018-03-21 20:11:24 -0400

    remove components that are replaced by reactstrap

--- client/src/components/Grid/Row.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export const Row = ({ fluid, id, children }) => (
-  <div id={id} className={`row${fluid ? '-fluid' : ''}`}>
-    {children}
-  </div>
-);
--- client/src/components/Jumbotron/Jumbotron.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Jumbotron = ({ children }) => (
-  <div style={{ marginBottom: '15px'}} className="jumbotron bg-dark text-white">
-    {children}
-  </div>
-);
-
-export default Jumbotron;
